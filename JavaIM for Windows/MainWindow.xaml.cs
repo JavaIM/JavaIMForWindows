833a1d7a6fb7b8f3c0046c469c589e2759d43b92
AlexLiuDev233 <wzylin11@outlook.com>
2024-09-18 20:33:40 +0800

    feat: 填加服务器&Token管理器 添加服务器对话框动画尚未完成，此对话框XAML准备改到自定义控件

--- JavaIM for Windows/MainWindow.xaml.cs
+++ JavaIM for Windows/MainWindow.xaml.cs
@@ -1,35 +1,44 @@
 using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.ApplicationSettings;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace JavaIM_for_Windows
 {
+    public class ServerInfo
+    {
+        public string ServerId { get; set; }
+        public string ServerToken { get; set; }
+        public string ServerName { get; set; }
+        public string ServerIP { get; set; }
+        public int ServerPort { get; set; }
+    }
     /// <summary>
     /// An empty window that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class MainWindow : Window
     {
+        public IntPtr Hwnd { get; }
+
+        private static MainWindow instance;
+        public static MainWindow GetInstance()
+        {
+            return instance;
+        }
+
+        public ServerInfo SelectServer { get; set; }
+
         public MainWindow()
         {
-            AppWindow m_AppWindow = this.AppWindow;
-            m_AppWindow.SetIcon("Assets/logo.ico");
-            this.InitializeComponent();
+            SelectServer = null;
+            instance = this;
+            AppWindow.SetIcon("Assets/logo.ico");
+            // Get the current window's HWND by passing in the Window object
+            Hwnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+            InitializeComponent();
         }
 
         private void NavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
--- JavaIM for Windows/Server.xaml.cs
+++ JavaIM for Windows/Server.xaml.cs
@@ -1,17 +1,13 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using System;
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Windows.Foundation.Metadata;
+using Windows.Storage;
+using Windows.Storage.Pickers;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -23,43 +19,208 @@ namespace JavaIM_for_Windows
     /// </summary>
     public sealed partial class Server : Page
     {
-        Button Button = new Button();
+        public ObservableCollection<ServerInfo> Servers { get; } = new ObservableCollection<ServerInfo>();
         // TextBox Text = new TextBox();
         public Server()
         {
-            Button.Content = "Add Server";
-            Button.Click += AddServerButton_Click;
-            this.InitializeComponent();
+            InitializeComponent();
+            _ = LoadServersAsync();
         }
 
+        public async Task LoadServersAsync()
+        {
+            Servers.Clear();
+            var task = GetServerListAsync();
+            if (task == null)
+                return;
+            foreach (var serverInfo in await task)
+            {
+                if (serverInfo == null)
+                    return;
+                Servers.Add(serverInfo);
+            }
+        }
+
+        
+        private async Task<StorageFolder> RequestDirectory(StorageFolder parentDirectory, String name)
+        {
+            StorageFolder dir;
+            try
+            {
+                dir = await parentDirectory.GetFolderAsync(name);
+            }
+            catch (FileNotFoundException)
+            {
+                try
+                {
+                    dir = await parentDirectory.CreateFolderAsync(name);
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    ContentDialog dialog = new ContentDialog
+                    {
+                        Title = "错误",
+                        Content = "无权创建" + name + "文件夹",
+                        CloseButtonText = "关闭",
+                        XamlRoot = XamlRoot
+                    };
+                    await dialog.ShowAsync();
+                    return null;
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                ContentDialog dialog = new ContentDialog
+                {
+                    Title = "错误",
+                    Content = "无权访问"+name+"文件夹",
+                    CloseButtonText = "关闭",
+                    XamlRoot = XamlRoot
+                };
+                await dialog.ShowAsync();
+                return null;
+            }// 处理文件夹
+            return await Task.FromResult(dir);
+        }
 
         private async void AddServerButton_Click(object sender, RoutedEventArgs e)
         {
+            Port.Value = double.NaN;
+            IP.Text = null;
+            Name.Text = null;
+            FileNameDisplay.Text = null;
+            serverCA = null;
             ContentDialogResult result = await AddServerContentDialog.ShowAsync();
             if (result == ContentDialogResult.Primary)
             {
-                // Terms of use were accepted.
-                Button.Content = "Accept";
+                var serverListTask = GetServerListAsync();
+                if (serverListTask == null)
+                    return;
+                foreach (var server in await serverListTask)
+                {
+                    if (server.ServerName.Equals(Name.Text))
+                    {
+                        
+                        ContentDialog dialog = new ContentDialog
+                        {
+                            Title = "错误",
+                            Content = "服务器名重复",
+                            CloseButtonText = "关闭",
+                            XamlRoot = XamlRoot
+                        };
+                        await dialog.ShowAsync();
+                        return;
+                    }
+                }
+                StorageFolder currentFolder = await StorageFolder.GetFolderFromPathAsync(Directory.GetCurrentDirectory());
+                
+                string serverId = Guid.NewGuid().ToString();// 写入crt+创建json
+                var task = RequestDirectory(currentFolder, "data");
+                if (task == null)
+                    return;
+                var folder = await task;
+                task = RequestDirectory(folder, "savedServers");
+                if (task == null)
+                    return;
+                var savedServerFolder = await task;
+                var asyncResult = savedServerFolder.CreateFileAsync(serverId + ".json");
+                StorageFile serverCert = await savedServerFolder.CreateFileAsync(serverId + ".crt");
+                StorageFile serverJson = await asyncResult;
+                await serverCA.CopyAndReplaceAsync(serverCert);
+                var serverInfo = new ServerInfo
+                {
+                    ServerName = Name.Text,
+                    ServerIP = IP.Text,
+                    ServerId = serverId,
+                    ServerToken = "",
+                    ServerPort = (int)Port.Value
+                };
+                string jsonString = JsonSerializer.Serialize(serverInfo);
+                await FileIO.WriteTextAsync(serverJson, jsonString);
+                await LoadServersAsync();
             }
+        }
+        private void RequestDialogPrimaryButtonUpdate()
+        {
+            if (string.IsNullOrEmpty(Name.Text) || string.IsNullOrEmpty(IP.Text) || double.IsNaN(Port.Value) || serverCA == null)
+                AddServerContentDialog.IsPrimaryButtonEnabled = false;
             else
+                AddServerContentDialog.IsPrimaryButtonEnabled = true;
+        }
+
+
+        private void Name_TextChanged(object sender, TextChangedEventArgs e) => RequestDialogPrimaryButtonUpdate();
+
+        private void Port_ValueChanged(NumberBox sender, NumberBoxValueChangedEventArgs args)
+        {
+            if (args.NewValue.ToString().Contains('.'))
+                Port.Value = args.OldValue;
+            RequestDialogPrimaryButtonUpdate();
+        }
+
+        private StorageFile serverCA;
+
+        private async void ChooseCACert_Click(object sender, RoutedEventArgs e)
+        {
+            var filePicker = new FileOpenPicker();
+            filePicker.FileTypeFilter.Add(".crt");
+            // Associate the HWND with the file picker
+            WinRT.Interop.InitializeWithWindow.Initialize(filePicker, MainWindow.GetInstance().Hwnd);
+            serverCA = await filePicker.PickSingleFileAsync();
+            if (serverCA == null)
+                return;
+            FileNameDisplay.Text = "CA证书文件名：" + serverCA.Name;
+            RequestDialogPrimaryButtonUpdate();
+        }
+
+        private void SelectServerButton_Click(object sender, RoutedEventArgs e)
+        {
+            // 获取点击的 Button
+            Button clickedButton = sender as Button;
+
+            // 从 Button 的 DataContext 获取对应的 Server 信息
+            var selectedServer = clickedButton?.DataContext as ServerInfo;
+            MainWindow.GetInstance().SelectServer = selectedServer;
+        }
+
+        private async Task<ObservableCollection<ServerInfo>> GetServerListAsync()
+        {
+            ObservableCollection<ServerInfo> servers = new ObservableCollection<ServerInfo>();
+            StorageFolder currentFolder = await StorageFolder.GetFolderFromPathAsync(Directory.GetCurrentDirectory());
+            var task = RequestDirectory(currentFolder, "data");
+            if (task == null)
+                return null;
+            var folder = await task;
+            task = RequestDirectory(folder, "savedServers");
+            if (task == null)
+                return null;
+            var savedServerFolder = await task;
+            var files = await savedServerFolder.GetFilesAsync();
+            foreach (var file in files)
             {
-                // User pressed Cancel, ESC, or the back arrow.
-                // Terms of use were not accepted.
-                Button.Content = "Accept";
+                if (!file.Name.EndsWith(".json"))
+                    continue;
+                try
+                {
+                    string jsonContent = await FileIO.ReadTextAsync(file);
+                    var server = JsonSerializer.Deserialize<ServerInfo>(jsonContent);
+                    servers.Add(server);
+                }
+                catch (Exception)
+                {
+                    ContentDialog dialog = new ContentDialog
+                    {
+                        Title = "错误",
+                        Content = "读取服务器列表时出错",
+                        CloseButtonText = "关闭",
+                        XamlRoot = XamlRoot
+                    };
+                    await dialog.ShowAsync();
+                    return null;
+                }
             }
+            return await Task.FromResult(servers);
         }
-
-        private void AddServerContentDialog_Opened(ContentDialog sender, ContentDialogOpenedEventArgs args)
-        {
-            // Ensure that the check box is unchecked each time the dialog opens.
-            //ConfirmAgeCheckBox.IsChecked = false;
-
-        }
-        private void inputBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            AddServerContentDialog.IsPrimaryButtonEnabled = !(string.IsNullOrEmpty(Name.Text) || string.IsNullOrEmpty(IP.Text) || string.IsNullOrEmpty(Port.Text));
-        }
-
     }
 
 }
